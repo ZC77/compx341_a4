8682726b6ad04523f8057979f9c6209fbc259a8a
ZC77 <zacharycui2009@gmail.com>
2021-06-02 21:07:41 +1200

    testing script 1

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -5,12 +5,12 @@ npm install
 npm run build
  
 if [ $? -eq 0 ]; then
-	echo "success!"
 	cd ..
 	git add *
 	git commit -m "$1"
 	git push origin master
 	git status
+	echo "success!"
 else
 	echo "Fail"
 fi
